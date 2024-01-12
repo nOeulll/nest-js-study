28f7035f7346d9780593285f07ab43d1339ad479
Hyeonjin Jeong <hjin@autocrypt.io>
2024-01-12 17:09:08 +0900

    [Pagination] Pagination 기본 ~ 심화 80프로 진행중

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,16 +1,19 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MoreThan, Repository } from 'typeorm';
+import { FindOptionsWhere, LessThan, MoreThan, Repository } from 'typeorm';
 import { PostsModel } from './entities/posts.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { PaginatePostDto } from './dto/paginate-post.dto';
+import { HOST, PROTOCOL } from '../common/const/env.const';
+import { CommonService } from '../common/common.service';
 
 @Injectable()
 export class PostsService {
   constructor(
     @InjectRepository(PostsModel)
     private readonly postsRepository: Repository<PostsModel>,
+    private readonly commonService: CommonService,
   ) {}
 
   async createPost(authorId: number, postDto: CreatePostDto) {
@@ -66,17 +69,97 @@ export class PostsService {
 
   // 1) 오름차순으로 정렬하는 pagination만 구현한다.
   async paginatePosts(dto: PaginatePostDto) {
-    const posts = await this.postsRepository.find({
-      where: {
-        // 더 크다. / 더 많다.
-        id: MoreThan(dto.where__id_more_than ?? 0),
+    return this.commonService.paginate(dto, this.postsRepository, {}, 'posts');
+    // if (dto.page) {
+    //   return this.pagePaginatePosts(dto);
+    // } else {
+    //   return this.cursorPaginatePosts(dto);
+    // }
+  }
+  async pagePaginatePosts(dto: PaginatePostDto) {
+    /**
+     * data: Data[],
+     * total: number,
+     * next: ??
+     *
+     * [1] [2] [3] [4]
+     */
+    const [posts, count] = await this.postsRepository.findAndCount({
+      skip: dto.take * (dto.page - 1),
+      take: dto.take,
+      order: {
+        createdAt: dto.order__createdAt,
       },
+    });
+
+    return {
+      data: posts,
+      total: count,
+    };
+  }
+
+  async cursorPaginatePosts(dto: PaginatePostDto) {
+    const where: FindOptionsWhere<PostsModel> = {};
+
+    if (dto.where__id__less_than) {
+      /**
+       * {
+       *   id: LessThan(dto.where__id_less_than)
+       * }
+       */
+      where.id = LessThan(dto.where__id__less_than);
+    } else if (dto.where__id__more_than) {
+      where.id = MoreThan(dto.where__id__more_than);
+    }
+    // 1, 2, 3, 4, 5
+    const posts = await this.postsRepository.find({
+      where,
       // order__createdAt
       order: {
         createdAt: dto.order__createdAt,
       },
       take: dto.take,
     });
+
+    // 해당되는 포스트가 0개 이상이면
+    // 마지막 포스트를 가져오고
+    // 아니면 null을 반환한다.
+    const lastItem =
+      posts.length > 0 && posts.length === dto.take
+        ? posts[posts.length - 1]
+        : null;
+
+    const nextUrl = lastItem && new URL(`${PROTOCOL}://${HOST}/posts`);
+
+    if (nextUrl) {
+      /**
+       * dto의 키 값들을 루핑하면서
+       * 키값에 해당되는 벨류가 존재하면
+       * param에 그대로 붙여넣는다.
+       *
+       * 단, where__id_more_than 값만 lastItem의 마지막 값으로 넣어준다.
+       */
+      for (const key of Object.keys(dto)) {
+        if (dto[key]) {
+          if (
+            'where__id__more_than' !== key &&
+            'where__id__less_than' !== key
+          ) {
+            nextUrl.searchParams.append(key, dto[key]);
+          }
+        }
+      }
+
+      let key = null;
+
+      if (dto.order__createdAt === 'ASC') {
+        key = 'where__id_more_than';
+      } else {
+        key = 'where__id_less_than';
+      }
+
+      nextUrl.searchParams.append(key, lastItem.id.toString());
+    }
 
     /**
      * Response
@@ -91,7 +174,9 @@ export class PostsService {
 
     return {
       data: posts,
+      cursor: { after: lastItem?.id ?? null },
       count: posts.length,
+      next: nextUrl?.toString() ?? null,
     };
   }
 
